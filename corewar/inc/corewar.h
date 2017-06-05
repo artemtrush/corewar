481b8986262d6eed19036c9bf0a54625035f9ed0
Oleg Chaychenko <ochayche@e1r7p13.unit.ua>
2017-06-05 19:41:44 +0300

    Makefile

---
+++ corewar/inc/corewar.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   corewar.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ochayche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/05 19:27:53 by ochayche          #+#    #+#             */
+/*   Updated: 2017/06/05 19:27:54 by ochayche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*		allowed function:
+**		open
+**		read
+**		lseek
+**		write
+**		close
+**		malloc
+**		realloc
+**		free
+**		perror
+**		strerror
+**		exit
+*/
+
+/* DO LIST:
+**
+**
+**
+*/
+
+#include <../../libft/inc/libft.h>
+#include <../../ft_printf/ft_printf.h>
+#include <../../inc/op.h>
