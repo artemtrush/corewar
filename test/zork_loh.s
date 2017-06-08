a439e3f92b1ebca5496d99e8cfd41ca50d543954
Oleg Chaychenko <ochayche@e1r7p13.unit.ua>
2017-06-08 20:17:22 +0300

    testing

--- test/zork_loh.s
+++ test/zork_loh.s
@@ -2,7 +2,7 @@
 .comment "I'M ALIIIIVE"
 
 l2:		sti r1, %:live, %1
-		and r1, %0, r1 # if (*r1 == 0) carry = 1;
+		# and r1, %0, r1 # if (*r1 == 0) carry = 1;
 		sti r1, %101, %0
 
 live:	live %1
---
+++ test/zork_test.s
@@ -0,0 +1,6 @@
+.name "zork_test"
+.comment "I'M ALIIIIVE"
+
+live %4294967295
+live %4294967294
+live %4294967293
